07f6de73e626ae4c33ef1f090688e19937aeb42f
Mariya Khan <mariyak9122@gmail.com>
2026-03-10 14:12:51 +0530

    Initial commit

--- app.py
+++ app.py
@@ -64,7 +64,9 @@ def analyze():
 if __name__ == "__main__":
     app.run(debug=True)
 
-
+if __name__ == "__main__":
+    import os
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
 # Runs web app
 # connects all modules
